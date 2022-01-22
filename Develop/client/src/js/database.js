57f6439a6e4a4eaf27610669a917d6bf8d7817c7
Jing Li <lijing900913@gmail.com>
2022-01-22 00:13:36 -0500

    Add StaleWhileRevalidate, and manifest.json

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -16,16 +16,16 @@ const initdb = async () =>
 export const putDb = async (content) => {
   console.log('Update the database');
   // Create a connection to the database database and version we want to use.
-  const editContentDb = await openDB('editContent', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = editContentDb.transaction('editContent', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('editContent');
+  const store = tx.objectStore('jate');
 
   // Use the .put() method to update the store and pass in the content.
-  const request = store.put(content, "editedContent");
+  const request = store.put({id:1, content: content});
 
   // Get confirmation of the request.
   const result = await request;
@@ -37,13 +37,13 @@ export const getDb = async () => {
   console.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
